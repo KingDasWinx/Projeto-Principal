91fd9ad27c98d9c5c7fa34dcba5f92221b17eaea
welinghton <welinghtonmarcelo@gmail.com>
2023-08-09 12:27:09 -0300

    COMEÇO DA SACOLA

---
+++ src/javascript/popup.js
@@ -0,0 +1,19 @@
+
+const abrir = document.querySelector(".bag-button")
+const popup = document.querySelector("dialog")
+const sair = document.querySelector(".sair")
+
+
+abrir.onclick = function (){
+  popup.showModal()
+}
+
+sair.onclick = function (){
+    popup.close()
+}
+
+popup.addEventListener('click', (event) => {
+    if (event.target === popup){
+        popup.close();
+    }
+})
